358e8052d08bbe097b96facb018369c818e951a3
Nikolas <58080745+Nikolas1320113201@users.noreply.github.com>
2021-03-09 16:26:41 +0200

    Update table users

--- Lab_2.sql
+++ Lab_2.sql
@@ -5,6 +5,27 @@ CREATE TABLE "users" (
   "country_code" int
 );
 
+insert into users (id, full_name, created_at, country_code) values (1, 'Peirce Primarolo', '4/27/2020', 'LT');
+insert into users (id, full_name, created_at, country_code) values (2, 'Mel Varley', '9/11/2020', 'UA');
+insert into users (id, full_name, created_at, country_code) values (3, 'Llywellyn Junkison', '7/30/2020', 'CY');
+insert into users (id, full_name, created_at, country_code) values (4, 'Casar Kegg', '10/16/2020', 'PK');
+insert into users (id, full_name, created_at, country_code) values (5, 'Pryce Michelmore', '7/8/2020', 'ID');
+insert into users (id, full_name, created_at, country_code) values (6, 'Wrennie Abbots', '3/25/2020', 'CL');
+insert into users (id, full_name, created_at, country_code) values (7, 'Aleen Beachem', '8/21/2020', 'FR');
+insert into users (id, full_name, created_at, country_code) values (8, 'Sig Coslett', '6/10/2020', 'ID');
+insert into users (id, full_name, created_at, country_code) values (9, 'Peria Axton', '11/18/2020', 'BR');
+insert into users (id, full_name, created_at, country_code) values (10, 'Frants Quye', '3/8/2021', 'SO');
+insert into users (id, full_name, created_at, country_code) values (11, 'Iago Thorneywork', '4/29/2020', 'CO');
+insert into users (id, full_name, created_at, country_code) values (12, 'Dayna Bride', '12/11/2020', 'CZ');
+insert into users (id, full_name, created_at, country_code) values (13, 'Trey Maletratt', '4/14/2020', 'HN');
+insert into users (id, full_name, created_at, country_code) values (14, 'Hew Kilmaster', '12/3/2020', 'PT');
+insert into users (id, full_name, created_at, country_code) values (15, 'Gothart Marmyon', '4/1/2020', 'ES');
+insert into users (id, full_name, created_at, country_code) values (16, 'Madella Jori', '12/8/2020', 'CN');
+insert into users (id, full_name, created_at, country_code) values (17, 'Nessy Whitehorn', '1/20/2021', 'PL');
+insert into users (id, full_name, created_at, country_code) values (18, 'Kordula Matherson', '9/30/2020', 'PH');
+insert into users (id, full_name, created_at, country_code) values (19, 'Wald Mairs', '8/28/2020', 'JP');
+insert into users (id, full_name, created_at, country_code) values (20, 'Creigh Elder', '6/14/2020', 'DK');
+
 CREATE TABLE "hookah" (
   "code" int PRIMARY KEY,
   "name" varchar,
